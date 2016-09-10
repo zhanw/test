cebc05094e02bbe4e446e9a8d8222c5c47f5bfe7
zhanw <258653667@qq.com>
2016-09-10 16:42:47 +0800

    push repository

---
+++ mylibrary/src/main/java/com/hyphenate/mylibrary/GradleMvnTest.java
@@ -0,0 +1,14 @@
+package com.hyphenate.mylibrary;
+
+/**
+ * Created by wei on 2016/9/10.
+ */
+public class GradleMvnTest {
+    public static void test1(){
+        System.out.println("test1");
+    }
+
+    public static void test2(){
+        System.out.println("test2");
+    }
+}
